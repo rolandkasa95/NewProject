39184efb43cb4cfb784af508e03672e46101c2e4
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-03 13:59:18 +0300

    Website1

---
+++ WebSite1/Cadredidactice.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    var referrer = document.referrer;
+    if (referrer == "http://localhost:8080/termeniisiconditii.html") {
+        $('#checkbox-navbar').prop('checked', true);
+
+});
+
---
+++ WebSite1/move_datas.php
@@ -0,0 +1,16 @@
+<?php
+$name="Fuckyou";
+echo 'Hello ' . htmlspecialchars($_POST["name"]) . '!';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
